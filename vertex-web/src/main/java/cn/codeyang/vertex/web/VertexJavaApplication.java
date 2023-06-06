7cc1e321c5fb13e8dadc472c000247eca7a04390
yangzy <306525121@qq.com>
2023-06-06 16:18:36 +0800

    feat: init

---
+++ vertex-web/src/main/java/cn/codeyang/vertex/web/VertexJavaApplication.java
@@ -0,0 +1,23 @@
+package cn.codeyang.vertex.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yangzy
+ */
+@SpringBootApplication
+public class VertexJavaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VertexJavaApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " ██      ██                  ██                  \n" +
+                "░██     ░██                 ░██                  \n" +
+                "░██     ░██  █████  ██████ ██████  █████  ██   ██\n" +
+                "░░██    ██  ██░░░██░░██░░█░░░██░  ██░░░██░░██ ██ \n" +
+                " ░░██  ██  ░███████ ░██ ░   ░██  ░███████ ░░███  \n" +
+                "  ░░████   ░██░░░░  ░██     ░██  ░██░░░░   ██░██ \n" +
+                "   ░░██    ░░██████░███     ░░██ ░░██████ ██ ░░██\n" +
+                "    ░░      ░░░░░░ ░░░       ░░   ░░░░░░ ░░   ░░ ");
+    }
+}
