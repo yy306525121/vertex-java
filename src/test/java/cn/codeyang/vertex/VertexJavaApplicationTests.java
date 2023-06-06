f9bcabdc397f3d62a93ac6e997367d3e8b570df4
yangzy <306525121@qq.com>
2023-06-06 16:01:10 +0800

    feat: init

---
+++ src/test/java/cn/codeyang/vertex/VertexJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.codeyang.vertex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VertexJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
